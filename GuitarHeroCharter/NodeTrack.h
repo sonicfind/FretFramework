c6cc83d03e147446e0258f74a6dd165fe9d1c768
sonicfind <52436993+sonicfind@users.noreply.github.com>
2022-01-16 11:37:47 -0600

    NodeTrack: separate events by difficulty

--- GuitarHeroCharter/NodeTrack.h
+++ GuitarHeroCharter/NodeTrack.h
@@ -18,33 +18,40 @@ class NodeTrack
 	{
 		std::map<uint32_t, T> m_notes;
 		std::map<uint32_t, Fret> m_starPower;
+		std::map<uint32_t, std::vector<std::string>> m_events;
+		void read_chart(std::stringstream& ss, const uint32_t position)
+		{
+			char type;
+			ss >> type;
+
+			if (type == 'E')
+			{
+				std::string line;
+				std::getline(ss, line);
+				m_events[position].push_back(std::move(line));
+			}
+			else
+			{
+				size_t lane;
+				uint32_t sustain;
+				ss >> lane >> sustain;
+
+				if (type == 'S')
+					m_starPower[position].init(sustain);
+				else
+					m_notes[position].init_chart(lane, sustain);
+			}
+		}
+
+		operator bool() const { return m_notes.size() || m_starPower.size() || m_events.size(); }
 	};
 	Difficulty m_difficulties[4];
-	std::map<uint32_t, std::vector<std::string>> m_localEvents;
+	
 public:
 	NodeTrack() = default;
 
 	void fromChart(std::stringstream& ss, DifficultyLevel difficulty, uint32_t position)
 	{
-		char type;
-		ss >> type;
-
-		if (type == 'E')
-		{
-			std::string line;
-			std::getline(ss, line);
-			m_localEvents[position].push_back(std::move(line));
-		}
-		else
-		{
-			size_t lane;
-			uint32_t sustain;
-			ss >> lane >> sustain;
-
-			if (type == 'S')
-				m_difficulties[static_cast<int>(difficulty)].m_starPower[position].init(sustain);
-			else
-				m_difficulties[static_cast<int>(difficulty)].m_notes[position].init_chart(lane, sustain);
-		}
+		m_difficulties[static_cast<int>(difficulty)].read_chart(ss, position);
 	}
 };
