bdbfccadd3f7ce22f59883709bc70f2bbd8bfd61
sonicfind <52436993+sonicfind@users.noreply.github.com>
2022-03-31 11:56:51 -0500

    Effect: getter function for matching midiNotes

--- GuitarHeroCharter/Song/Effect.cpp
+++ GuitarHeroCharter/Song/Effect.cpp
@@ -7,9 +7,19 @@ void StarPowerPhrase::save_chart(uint32_t position, std::fstream& outFile)
 	outFile << "  " << position << " = S " << m_duration << '\n';
 }
 
+char StarPowerPhrase::getMidiNote() const
+{
+	return 116;
+}
+
 void StarPowerActivation::save_chart(uint32_t position, std::fstream& outFile)
 {
 	outFile << "  " << position << " = A " << m_duration << '\n';
+}
+
+char StarPowerActivation::getMidiNote() const
+{
+	return 0;
 }
 
 void Tremolo::save_chart(uint32_t position, std::fstream& outFile)
@@ -17,7 +27,17 @@ void Tremolo::save_chart(uint32_t position, std::fstream& outFile)
 	outFile << "  " << position << " = R " << m_duration << '\n';
 }
 
+char Tremolo::getMidiNote() const
+{
+	return 126;
+}
+
 void Trill::save_chart(uint32_t position, std::fstream& outFile)
 {
 	outFile << "  " << position << " = D " << m_duration << '\n';
+}
+
+char Trill::getMidiNote() const
+{
+	return 127;
 }
--- GuitarHeroCharter/Song/Effect.h
+++ GuitarHeroCharter/Song/Effect.h
@@ -11,18 +11,21 @@ public:
 	uint32_t getDuration() const { return m_duration; }
 	virtual void save_chart(uint32_t position, std::fstream& outFile) = 0;
 	virtual ~Effect() {}
+	virtual char getMidiNote() const = 0;
 };
 
 class StarPowerPhrase : public Effect
 {
 	using Effect::Effect;
 	void save_chart(uint32_t position, std::fstream& outFile);
+	char getMidiNote() const;
 };
 
 class StarPowerActivation : public Effect
 {
 	using Effect::Effect;
 	void save_chart(uint32_t position, std::fstream& outFile);
+	char getMidiNote() const;
 };
 
 // AKA rolls
@@ -30,6 +33,7 @@ class Tremolo : public Effect
 {
 	using Effect::Effect;
 	void save_chart(uint32_t position, std::fstream& outFile);
+	char getMidiNote() const;
 };
 
 // AKA Special rolls (multiple hands/frets)
@@ -37,4 +41,5 @@ class Trill : public Effect
 {
 	using Effect::Effect;
 	void save_chart(uint32_t position, std::fstream& outFile);
+	char getMidiNote() const;
 };
