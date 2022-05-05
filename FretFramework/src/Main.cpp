cb251fe23a944e8e8c6110138b4f70473d09869d
sonicfind <52436993+sonicfind@users.noreply.github.com>
2022-05-05 11:31:59 -0500

    Add import speed test functionality

--- FretFramework/src/Main.cpp
+++ FretFramework/src/Main.cpp
@@ -5,6 +5,11 @@ int main()
 {
 	std::string filename;
 	std::getline(std::cin, filename);
+	std::transform(filename.begin(), filename.end(), filename.begin(),
+					[](unsigned char c) { return std::tolower(c); });
+	bool doTest = filename == "test";
+
+	std::getline(std::cin, filename);
 	if (filename[0] == '\"')
 		filename = filename.substr(1, filename.length() - 2);
 
@@ -12,8 +17,27 @@ int main()
 
 	try
 	{
-		Song song(path);
-		song.save();
+		if (!doTest)
+		{
+			Song song(path);
+			song.save();
+		}
+		else
+		{
+			long long total = 0;
+			int i = 0;
+			for (; i < 10000 && total < 60000000; ++i)
+			{
+				auto t1 = std::chrono::high_resolution_clock::now();
+				Song song(path);
+				auto t2 = std::chrono::high_resolution_clock::now();
+				long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+				std::cout << "import test " << i + 1 << " took " << count / 1000.0 << " milliseconds\n";
+				total += count;
+			}
+			std::cout << "import test took " << total / 1000 << " milliseconds\n";
+			std::cout << "each import took " << total / double(i * 1000) << " milliseconds on average\n";
+		}
 	}
 	catch (EndofFileException EoF)
 	{
