f5317406340686a2c3926d1589f31c4202f2a914
sonicfind <52436993+sonicfind@users.noreply.github.com>
2022-05-08 07:25:30 -0500

    Introduce wrapper for text file reads
    This new TextTraversal class will handle traversing through a text file byte by byte in memory instead of through constantly line by line I/O calls. This style will save a substantial amount of time through the removal of the I/O calls and through the removal of needless memory copying.

---
+++ FretFramework/src/TextFileManip.cpp
@@ -0,0 +1,112 @@
+#include "TextFileManip.h"
+
+TextTraversal::TextTraversal(const std::filesystem::path& path)
+{
+	std::fstream inFile = FilestreamCheck::getFileStream(path, std::ios_base::in);
+	inFile.seekg(0, inFile.end);
+	size_t length = inFile.tellg();
+	inFile.seekg(0, inFile.beg);
+
+	m_file = new char[length + 1]();
+	m_end = m_file + length;
+	inFile.read((char*)m_file, length);
+	inFile.close();
+
+	m_current = m_file;
+	if (!(m_next = strchr(m_current, '\n')))
+		m_next = m_end - 1;
+}
+
+void TextTraversal::skipWhiteSpace()
+{
+	while (m_current < m_end && m_current < m_next)
+	{
+		switch (*m_current)
+		{
+		case ' ':
+		case '\t':
+			++m_current;
+			break;
+		default:
+			return;
+		}
+	}
+}
+
+void TextTraversal::nextLine()
+{
+	m_current = m_next + 1;
+	if (!(m_next = strchr(m_current, '\n')))
+		m_next = m_end - 1;
+	skipWhiteSpace();
+}
+
+void TextTraversal::skipScope()
+{
+	int scopeTracker = 1;
+	while (*this && scopeTracker > 0)
+	{
+		if (*m_current == '}')
+			--scopeTracker;
+		else
+		{
+			if (*m_current == '{')
+				++scopeTracker;
+			nextLine();
+		}
+	}
+}
+
+void TextTraversal::move(size_t count)
+{
+	m_current += count;
+	if (m_current > m_end)
+		m_current = m_end;
+	else
+		skipWhiteSpace();
+}
+
+std::string_view TextTraversal::extractText()
+{
+	if (*m_current == '\"')
+	{
+		const char* test = m_next - 1;
+		while (test > m_current)
+		{
+			if (*test == '\"')
+			{
+				std::string_view str(m_current + 1, test - m_current - 1);
+				m_current = test + 1;
+				skipWhiteSpace();
+				return str;
+			}
+		}
+	}
+
+	std::string_view str(m_current, m_next - m_current);
+	m_current = m_next;
+	return str;
+}
+
+size_t TextTraversal::extract(long& value)
+{
+	const char* after;
+	value = strtol(m_current, (char**)&after, 0);
+	if (after > m_next)
+		after = m_current;
+	return after - m_current;
+}
+
+size_t TextTraversal::extract(uint32_t& value)
+{
+	const char* after;
+	value = strtoul(m_current, (char**)&after, 0);
+	if (after > m_next)
+		after = m_current;
+	return after - m_current;
+}
+
+TextTraversal::~TextTraversal()
+{
+	delete[m_end - m_file + 1] m_file;
+}
---
+++ FretFramework/src/TextFileManip.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "NoteExceptions.h"
+#include "FilestreamCheck.h"
+#include <filesystem>
+#include <string_view>
+
+class TextTraversal
+{
+	const char* m_file;
+	const char* m_end;
+	const char* m_current;
+	const char* m_next;
+	size_t m_textLength = 0;
+public:
+	TextTraversal(const std::filesystem::path& path);
+	void skipWhiteSpace();
+	void nextLine();
+	void skipScope();
+	void move(size_t count);
+	std::string_view extractText();
+	size_t extract(long& value);
+	size_t extract(uint32_t& value);
+	~TextTraversal();
+
+	const char* getCurrent() { return m_current; }
+	const char getChar() { return *m_current; }
+	
+	size_t getLineLength() { return m_next - m_current; }
+
+	bool operator==(char c) { return *m_current == c; }
+	bool operator!=(char c) { return *m_current != c; }
+	operator bool() { return m_current && m_current < m_end; }
+};
