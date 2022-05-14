55e58f1cafa7b4f04480bb257b4cc08ed950e216
sonicfind <52436993+sonicfind@users.noreply.github.com>
2022-05-14 00:35:20 -0500

    Vocal: apply max lyric length of 255

--- FretFramework/src/Song/Notes/Vocals/Vocal.cpp
+++ FretFramework/src/Song/Notes/Vocals/Vocal.cpp
@@ -2,7 +2,10 @@
 
 void Vocal::setLyric(const std::string& text)
 {
-	m_lyric = text;
+	if (m_lyric.length() <= 255)
+		m_lyric = text;
+	else
+		m_lyric = text.substr(0, 255);
 }
 
 void Vocal::save_cht(int lane, std::fstream& outFile) const
