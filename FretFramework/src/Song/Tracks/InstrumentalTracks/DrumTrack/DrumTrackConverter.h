c5edeb626d90d3122e5bbcff17504bf4fa106334
sonicfind <sonicfind2@gmail.com>
2022-06-06 22:31:18 -0500

    DrumTrackConverter: templatize static function

--- FretFramework/src/Song/Tracks/InstrumentalTracks/DrumTrack/DrumTrackConverter.h
+++ FretFramework/src/Song/Tracks/InstrumentalTracks/DrumTrack/DrumTrackConverter.h
@@ -4,9 +4,24 @@
 
 class DrumTrackConverter
 {
-	static std::pair<uint32_t, DrumNote<4, DrumPad_Pro>> s_4LanePair;
-	static std::pair<uint32_t, DrumNote<5, DrumPad>> s_5LanePair;
 public:
-	static void convert(InstrumentalTrack<DrumNote_Legacy>& legacy, InstrumentalTrack<DrumNote<4, DrumPad_Pro>>* track);
-	static void convert(InstrumentalTrack<DrumNote_Legacy>& legacy, InstrumentalTrack<DrumNote<5, DrumPad>>* track);
+	template <class DrumNoteType>
+	static void convert(InstrumentalTrack<DrumNote_Legacy>& legacy, InstrumentalTrack<DrumNoteType>* track)
+	{
+		std::pair<uint32_t, DrumNoteType> drumPair;
+		for (int i = 0; i < 5; ++i)
+			if (legacy.m_difficulties[i].occupied())
+			{
+				track->m_difficulties[i].m_effects = std::move(legacy.m_difficulties[i].m_effects);
+				track->m_difficulties[i].m_events = std::move(legacy.m_difficulties[i].m_events);
+				track->m_difficulties[i].m_notes.reserve(legacy.m_difficulties[i].m_notes.size());
+				track->m_difficulties[i].m_notes.clear();
+				for (const auto& pair : legacy.m_difficulties[i].m_notes)
+				{
+					drumPair.first = pair.first;
+					pair.second.convert(drumPair.second);
+					track->m_difficulties[i].m_notes.push_back(drumPair);
+				}
+			}
+	}
 };
