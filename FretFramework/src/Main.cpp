4b09f81486e1b705bacfa2406f398b0dcf93977e
sonicfind <sonicfind2@gmail.com>
2022-06-11 11:25:20 -0500

    Main: redone commandline menu setup
    Rearranged for scan testing

--- FretFramework/src/Main.cpp
+++ FretFramework/src/Main.cpp
@@ -1,11 +1,18 @@
 #include "Song/Song.h"
 #include "FileChecks/FilestreamCheck.h"
 #include <iostream>
+
+void load(std::string& filename);
+void benchmark();
+
+void scan();
+void scanBenchmark();
+
 int main()
 {
 	while (true)
 	{
-		std::cout << "Drag and drop a file to the console (type \"test\" for a loop import benchmark or \"quit\" to exit the app): ";
+		std::cout << "Drag and drop a file to the console (type \"test\" for a loop import benchmark, \"scan\" for chart validation, or \"quit\" to exit the app): ";
 		std::string filename;
 		std::getline(std::cin, filename);
 		std::transform(filename.begin(), filename.end(), filename.begin(),
@@ -18,39 +25,11 @@ int main()
 		try
 		{
 			if (filename == "test")
-			{
-				std::cout << "Benchmark Mode - Drag and drop a file to the console: ";
-				std::getline(std::cin, filename);
-				if (filename[0] == '\"')
-					filename = filename.substr(1, filename.length() - 2);
-
-				path = filename;
-
-				long long total = 0;
-				int i = 0;
-				for (; i < 10000 && total < 60000000; ++i)
-				{
-					Song song;
-					auto t1 = std::chrono::high_resolution_clock::now();
-					song.load(path);
-					auto t2 = std::chrono::high_resolution_clock::now();
-					long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-					std::cout << "import test " << i + 1 << " took " << count / 1000.0 << " milliseconds\n";
-					total += count;
-				}
-				std::cout << "import test took " << total / 1000 << " milliseconds\n";
-				std::cout << "each import took " << total / (i * 1000.0f) << " milliseconds on average\n";
-			}
+				benchmark();
+			else if (filename == "scan")
+				scan();
 			else
-			{
-				if (filename[0] == '\"')
-					filename = filename.substr(1, filename.length() - 2);
-
-				path = filename;
-				Song song(path);
-				song.save();
-				std::getline(std::cin, filename);
-			}
+				load(filename);
 		}
 		catch (std::runtime_error err)
 		{
@@ -59,4 +38,96 @@ int main()
 	}
 	Song::deleteTracks();
 	return 0;
+}
+
+void benchmark()
+{
+	std::cout << "Benchmark Mode - Drag and drop a file to the console (type \"quit\" to exit to main loop): ";
+	std::string filename;
+	std::getline(std::cin, filename);
+	if (filename[0] == '\"')
+		filename = filename.substr(1, filename.length() - 2);
+
+	if (filename == "quit")
+		return;
+
+	std::filesystem::path path = filename;
+	long long total = 0;
+	int i = 0;
+	for (; i < 10000 && total < 60000000; ++i)
+	{
+		Song song;
+		auto t1 = std::chrono::high_resolution_clock::now();
+		song.load(path);
+		auto t2 = std::chrono::high_resolution_clock::now();
+		long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+		std::cout << "import test " << i + 1 << " took " << count / 1000.0 << " milliseconds\n";
+		total += count;
+	}
+	std::cout << "import test took " << total / 1000 << " milliseconds\n";
+	std::cout << "each import took " << total / (i * 1000.0f) << " milliseconds on average\n";
+}
+
+void scan()
+{
+	std::cout << "Scan Mode - Drag and drop a file to the console (type \"test\" for a loop scan benchmark or \"quit\" to exit to main loop: ";
+	std::string filename;
+	std::getline(std::cin, filename);
+	std::transform(filename.begin(), filename.end(), filename.begin(),
+		[](unsigned char c) { return std::tolower(c); });
+
+	if (filename == "test")
+	{
+		scanBenchmark();
+		return;
+	}
+
+	if (filename == "quit")
+		return;
+
+	if (filename[0] == '\"')
+		filename = filename.substr(1, filename.length() - 2);
+
+	std::filesystem::path path(filename);
+	Song song;
+	song.scan(path);
+}
+
+void scanBenchmark()
+{
+	std::cout << "Scan Benchmark Mode - Drag and drop a file to the console (type \"quit\" to exit to main loop): ";
+	std::string filename;
+	std::getline(std::cin, filename);
+	if (filename[0] == '\"')
+		filename = filename.substr(1, filename.length() - 2);
+
+	if (filename == "quit")
+		return;
+
+	std::filesystem::path path = filename;
+	long long total = 0;
+	int i = 0;
+	for (; i < 10000 && total < 60000000; ++i)
+	{
+		Song song;
+		auto t1 = std::chrono::high_resolution_clock::now();
+		song.scan(path);
+		auto t2 = std::chrono::high_resolution_clock::now();
+		long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+		std::cout << "import test " << i + 1 << " took " << count / 1000.0 << " milliseconds\n";
+		total += count;
+	}
+	std::cout << "import test took " << total / 1000 << " milliseconds\n";
+	std::cout << "each import took " << total / (i * 1000.0f) << " milliseconds on average\n";
+}
+
+void load(std::string& filename)
+{
+	if (filename[0] == '\"')
+		filename = filename.substr(1, filename.length() - 2);
+
+	std::filesystem::path path(filename);
+	Song song(path);
+	song.save();
+	std::getline(std::cin, filename);
 }
