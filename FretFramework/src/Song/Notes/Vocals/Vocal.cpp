aafb229b2074039cc05aa1389b52eaf0dbe62029
sonicfind <sonicfind2@gmail.com>
2022-06-12 11:28:27 -0500

    Vocal: add traversal init functions
    Gives it matching behavior to the InstrumentalNote type

--- FretFramework/src/Song/Notes/Vocals/Vocal.cpp
+++ FretFramework/src/Song/Notes/Vocals/Vocal.cpp
@@ -1,5 +1,42 @@
 #include "Vocal.h"
 
+void Vocal::init(TextTraversal& traversal)
+{
+	try
+	{
+		setLyric(traversal.extractLyric());
+		if (uint32_t pitch; traversal.extract(pitch))
+		{
+			m_pitch = (char)pitch;
+			init(traversal.extractU32());
+		}
+	}
+	catch (Traversal::NoParseException)
+	{
+		throw EndofLineException();
+	}
+}
+
+void Vocal::init(BCHTraversal& traversal)
+{
+	try
+	{
+		unsigned char length = traversal.extractChar();
+		setLyric(traversal.extractLyric(length));
+
+		// Read pitch
+		if (unsigned char pitch; traversal.extract(pitch))
+		{
+			m_pitch = pitch;
+			init(traversal.extractVarType());
+		}
+	}
+	catch (Traversal::NoParseException)
+	{
+		throw EndofEventException();
+	}
+}
+
 void Vocal::setLyric(const std::string& text)
 {
 	if (m_lyric.length() <= 255)
--- FretFramework/src/Song/Notes/Vocals/Vocal.h
+++ FretFramework/src/Song/Notes/Vocals/Vocal.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "Base Nodes/Pitched.h"
+#include "NoteExceptions.h"
+#include "FileTraversal/TextFileTraversal.h"
+#include "FileTraversal/BCHFileTraversal.h"
 
 // m_isActive will be used to determine whether the note is "singable"
 class Vocal : public Pitched
 {
 	std::string m_lyric;
 public:
+	using Pitched::init;
+	void init(TextTraversal& traversal);
+	void init(BCHTraversal& traversal);
 	void setLyric(const std::string& text);
 	std::string getLyric() const { return m_lyric; }
 	void save_cht(int lane, std::stringstream& buffer) const;
