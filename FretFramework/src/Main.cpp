40602746580b4fc7c92e235b77cd9df69c9da8a3
sonicfind <sonicfind2@gmail.com>
2022-06-21 01:47:37 -0500

    Main: use variable to toggle "loop benchmark"
    + Cease the need for separate functions for that functionality

--- FretFramework/src/Main.cpp
+++ FretFramework/src/Main.cpp
@@ -4,14 +4,12 @@
 #include <list>
 #include <iostream>
 
-void benchmark();
-
 void scan();
-void scanBenchmark();
 void fullScan();
 
 // Cache saving is not yet implemented so no path is given
 SongCache g_songCache{ std::filesystem::path() };
+bool g_benchmark = false;
 
 int main()
 {
@@ -20,7 +18,8 @@ int main()
 	std::locale::global(std::locale(localeName));
 	while (true)
 	{
-		std::cout << "Drag and drop a file to the console (type \"test\" for a loop import benchmark, \"scan\" for chart validation, or \"quit\" to exit the app): ";
+		std::cout << "Drag and drop a file to the console (type \"loop\" to toggle a loop benchmark, \"scan\" for chart validation, or \"quit\" to exit the app)\n";
+		std::cout << "Loop Benchmark: " << (g_benchmark ? "Enabled" : "Disabled") << "\nInput: ";
 		std::string filename;
 		std::getline(std::cin, filename);
 		std::transform(filename.begin(), filename.end(), filename.begin(),
@@ -31,11 +30,15 @@ int main()
 
 		std::cout << std::endl;
 
+		if (filename == "loop")
+		{
+			g_benchmark = !g_benchmark;
+			continue;
+		}
+
 		try
 		{
-			if (filename == "test")
-				benchmark();
-			else if (filename == "scan")
+			if (filename == "scan")
 				scan();
 			else
 			{
@@ -43,9 +46,30 @@ int main()
 					filename = filename.substr(1, filename.length() - 2);
 
 				std::filesystem::path path(filename);
-				Song song(path);
-				song.save();
-				std::getline(std::cin, filename);
+				if (!g_benchmark)
+				{
+					Song song(path);
+					song.save();
+					std::getline(std::cin, filename);
+				}
+				else
+				{
+					long long total = 0;
+					int i = 0;
+					for (; i < 10000 && total < 60000000; ++i)
+					{
+						Song song;
+						auto t1 = std::chrono::high_resolution_clock::now();
+						song.load(path);
+						auto t2 = std::chrono::high_resolution_clock::now();
+						long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+						std::cout << "Load test " << i + 1 << " took " << count / 1000.0 << " milliseconds\n";
+						total += count;
+					}
+					std::cout << "Load test took " << total / 1000 << " milliseconds\n";
+					std::cout << "Each load took " << total / (i * 1000.0f) << " milliseconds on average\n";
+					std::cout << std::endl;
+				}
 			}
 		}
 		catch (std::runtime_error err)
@@ -59,53 +83,22 @@ int main()
 	return 0;
 }
 
-void benchmark()
-{
-	while (true)
-	{
-		std::cout << "Benchmark Mode - Drag and drop a file to the console (type \"quit\" to exit to main loop): ";
-		std::string filename;
-		std::getline(std::cin, filename);
-		if (filename[0] == '\"')
-			filename = filename.substr(1, filename.length() - 2);
-
-		if (filename == "quit")
-			break;
-
-		std::filesystem::path path = filename;
-		long long total = 0;
-		int i = 0;
-		for (; i < 10000 && total < 60000000; ++i)
-		{
-			Song song;
-			auto t1 = std::chrono::high_resolution_clock::now();
-			song.load(path);
-			auto t2 = std::chrono::high_resolution_clock::now();
-			long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-			std::cout << "import test " << i + 1 << " took " << count / 1000.0 << " milliseconds\n";
-			total += count;
-		}
-		std::cout << "import test took " << total / 1000 << " milliseconds\n";
-		std::cout << "each import took " << total / (i * 1000.0f) << " milliseconds on average\n";
-		std::cout << std::endl;
-	}
-}
-
 void scan()
 {
 	while (true)
 	{
-		std::cout << "Scan Mode - Drag and drop a file to the console (type \"test\" for a loop scan benchmark, \"full\" for a directory scan test, or \"quit\" to exit to main loop): ";
+		std::cout << "Scan Mode - Drag and drop a file to the console (type \"loop\" to toggle a loop benchmark, \"full\" for a directory scan test, or \"quit\" to exit to main loop)\n";
+		std::cout << "Loop Benchmark: " << (g_benchmark ? "Enabled" : "Disabled") << "\nInput: ";
 		std::string filename;
 		std::getline(std::cin, filename);
 		std::transform(filename.begin(), filename.end(), filename.begin(),
 			[](unsigned char c) { return std::tolower(c); });
 
-		if (filename == "test")
+		if (filename == "loop")
 		{
+			g_benchmark = !g_benchmark;
 			std::cout << std::endl;
-			scanBenchmark();
-			break;
+			continue;
 		}
 
 		if (filename == "full")
@@ -124,42 +117,32 @@ void scan()
 			filename = filename.substr(1, filename.length() - 2);
 
 		std::filesystem::path path(filename);
-		SongScan song;
-		song.scan(path);
-		SongScan::waitForHasher();
-		song.getHash().display();
-	}
-}
-
-void scanBenchmark()
-{
-	while (true)
-	{
-		std::cout << "Scan Benchmark Mode - Drag and drop a file to the console (type \"quit\" to exit to main loop): ";
-		std::string filename;
-		std::getline(std::cin, filename);
-		if (filename[0] == '\"')
-			filename = filename.substr(1, filename.length() - 2);
-
-		if (filename == "quit")
-			break;
-
-		std::filesystem::path path = filename;
-		long long total = 0;
-		int i = 0;
-		for (; i < 10000 && total < 60000000; ++i)
+		if (!g_benchmark)
 		{
 			SongScan song;
-			auto t1 = std::chrono::high_resolution_clock::now();
 			song.scan(path);
-			auto t2 = std::chrono::high_resolution_clock::now();
-			long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-			std::cout << "import test " << i + 1 << " took " << count / 1000.0 << " milliseconds\n";
-			total += count;
+			SongScan::waitForHasher();
+			song.getHash().display();
 		}
-		std::cout << "import test took " << total / 1000 << " milliseconds\n";
-		std::cout << "each import took " << total / (i * 1000.0f) << " milliseconds on average\n";
-		std::cout << std::endl;
+		else
+		{
+			long long total = 0;
+			int i = 0;
+			for (; i < 10000 && total < 60000000; ++i)
+			{
+				SongScan song;
+				auto t1 = std::chrono::high_resolution_clock::now();
+				song.scan(path);
+				SongScan::waitForHasher();
+				auto t2 = std::chrono::high_resolution_clock::now();
+				long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+				std::cout << "Scan test " << i + 1 << " took " << count / 1000.0 << " milliseconds\n";
+				total += count;
+			}
+			std::cout << "Scan test took " << total / 1000 << " milliseconds\n";
+			std::cout << "Each scan took " << total / (i * 1000.0f) << " milliseconds on average\n";
+			std::cout << std::endl;
+		}
 	}
 }
 
@@ -168,7 +151,8 @@ void fullScan()
 	while (true)
 	{
 		std::vector<std::filesystem::path> directories;
-		std::cout << "Full Scan Mode - Drag and drop a directory to the console (type \"multi\" to input multiple directories or \"quit\" to exit to main loop)\n";
+		std::cout << "Full Scan Mode - Drag and drop a directory to the console (type \"loop\" to toggle a loop benchmark, \"multi\" to input multiple directories, or \"quit\" to exit to main loop)\n";
+		std::cout << "Loop Benchmark: " << (g_benchmark ? "Enabled\n" : "Disabled\n");
 		std::cout << "Duplicates " << (g_songCache.areDuplicatesAllowed() ? "allowed" : "disallowed") << " (\"toggle\" to change this setting)\n";
 		std::cout << "Input: ";
 		std::string filename;
@@ -178,6 +162,13 @@ void fullScan()
 
 		if (filename == "quit")
 			break;
+
+		if (filename == "loop")
+		{
+			g_benchmark = !g_benchmark;
+			std::cout << std::endl;
+			continue;
+		}
 
 		if (filename == "toggle")
 		{
@@ -190,7 +181,8 @@ void fullScan()
 		{
 			while (true)
 			{
-				std::cout << "Multi-Directory Scan Mode - Drag and drop a directory to the console (type \"done\" when all directories are added or \"quit\" to exit to main loop): ";
+				std::cout << "Multi-Directory Scan Mode - Drag and drop a directory to the console (type \"loop\" to toggle a loop benchmark, \"done\" when all directories are added, or \"quit\" to exit to main loop)\n";
+				std::cout << "Loop Benchmark: " << (g_benchmark ? "Enabled\n" : "Disabled\n");
 				std::cout << "Duplicates " << (g_songCache.areDuplicatesAllowed() ? "allowed" : "disallowed") << " (\"toggle\" to change this setting)\n";
 				std::cout << "Input: ";
 				std::string filename;
@@ -201,6 +193,13 @@ void fullScan()
 
 				if (filename == "quit")
 					return;
+
+				if (filename == "loop")
+				{
+					g_benchmark = !g_benchmark;
+					std::cout << std::endl;
+					continue;
+				}
 
 				if (filename == "toggle")
 				{
@@ -221,13 +220,35 @@ void fullScan()
 
 		std::cout << std::endl;
 
-		auto t1 = std::chrono::high_resolution_clock::now();
-		g_songCache.scan(directories);
-		auto t2 = std::chrono::high_resolution_clock::now();
+		if (!g_benchmark)
+		{
+			auto t1 = std::chrono::high_resolution_clock::now();
+			g_songCache.scan(directories);
+			auto t2 = std::chrono::high_resolution_clock::now();
 
-		long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-		std::cout << "Full scan complete - # of songs: " << g_songCache.getNumSongs() << std::endl;
-		std::cout << "Time taken: " << count / 1000 << " milliseconds\n";
-		std::cout << std::endl;
+			long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+			std::cout << "Full scan complete - # of songs: " << g_songCache.getNumSongs() << std::endl;
+			std::cout << "Time taken: " << count / 1000 << " milliseconds\n";
+			std::cout << std::endl;
+		}
+		else
+		{
+			long long total = 0;
+			int i = 0;
+			for (; i < 10000 && total < 60000000; ++i)
+			{
+				auto t1 = std::chrono::high_resolution_clock::now();
+				g_songCache.scan(directories);
+				auto t2 = std::chrono::high_resolution_clock::now();
+
+				long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+				std::cout << "Full scan " << i + 1 << " took " << count / 1000.0 << " milliseconds\n";
+				total += count;
+			}
+
+			std::cout << "Full Scan test took " << total / 1000 << " milliseconds\n";
+			std::cout << "Each full scan took " << total / (i * 1000.0f) << " milliseconds on average\n";
+			std::cout << std::endl;
+		}
 	}
 }
