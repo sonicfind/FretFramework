c6c5244905a11f4af8433aac6c82168a815d9fe8
sonicfind <sonicfind2@gmail.com>
2022-07-18 11:03:33 -0500

    Thread function location reordering

--- FretFramework/src/FileHasher/FileHasher.cpp
+++ FretFramework/src/FileHasher/FileHasher.cpp
@@ -11,27 +11,6 @@ FileHasher::~FileHasher()
 	stopThreads();
 }
 
-void FileHasher::startThreads()
-{
-	m_status = ACTIVE;
-	for (unsigned int i = 0; i < m_threadCount; ++i)
-		m_threads.emplace_back(&FileHasher::hashThread, this);
-}
-
-void FileHasher::stopThreads()
-{
-	if (m_status == ACTIVE)
-	{
-		m_status = INACTIVE;
-		m_condition.notify_all();
-
-		for (unsigned int i = 0; i < m_threadCount; ++i)
-			m_threads[i].join();
-
-		m_threads.clear();
-	}
-}
-
 void FileHasher::addNode(std::shared_ptr<MD5>& hash, std::shared_ptr<FilePointers>& filePointers)
 {
 	m_queue.push({ hash, filePointers });
@@ -55,5 +34,26 @@ void FileHasher::hashThread()
 			HashNode& node = opt.value();
 			node.hash->generate(node.file->m_file, node.file->m_end);
 		}
+	}
+}
+
+void FileHasher::startThreads()
+{
+	m_status = ACTIVE;
+	for (unsigned int i = 0; i < m_threadCount; ++i)
+		m_threads.emplace_back(&FileHasher::hashThread, this);
+}
+
+void FileHasher::stopThreads()
+{
+	if (m_status == ACTIVE)
+	{
+		m_status = INACTIVE;
+		m_condition.notify_all();
+
+		for (unsigned int i = 0; i < m_threadCount; ++i)
+			m_threads[i].join();
+
+		m_threads.clear();
 	}
 }
