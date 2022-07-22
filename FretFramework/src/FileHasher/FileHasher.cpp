d6d65803d4b9dbd4e642ad2fe4c3470d7f301ab9
sonicfind <sonicfind2@gmail.com>
2022-07-22 10:43:20 -0500

    Address substantial performance drop from threading change
    The issue stems from having to call SafeQueue::pop_front() after being placed in a "Stop" state. That alone takes a drastic amount of time. We can address this by internalizing the thread waiting period into pop_front() directly.
    
    + In doing so, we can simplify ThreadSet arrays into arrays of statuses (which have been converted to unique_ptr).

--- FretFramework/src/FileHasher/FileHasher.cpp
+++ FretFramework/src/FileHasher/FileHasher.cpp
@@ -2,77 +2,59 @@
 
 FileHasher::FileHasher()
 	: m_threadCount(std::thread::hardware_concurrency() >= 4 ? std::thread::hardware_concurrency() / 2 : 1)
+	, m_statuses(std::make_unique<std::atomic<ThreadStatus>[]>(m_threadCount))
 {
-	m_threadSets = new ThreadSet[m_threadCount];
 	m_threads.reserve(m_threadCount);
 	for (unsigned int i = 0; i < m_threadCount; ++i)
-		m_threads.emplace_back(&FileHasher::hashThread, this, std::ref(m_threadSets[i]));
+		m_threads.emplace_back(&FileHasher::hashThread, this, std::ref(m_statuses[i]));
 }
 
 FileHasher::~FileHasher()
 {
 	stopThreads();
+
 	for (unsigned int i = 0; i < m_threadCount; ++i)
-	{
-		m_threadSets[i].status = ThreadSet::QUIT;
-		m_threadSets[i].idleCondition.notify_one();
-	}
+		m_statuses[i] = QUIT;
+	m_condition.notify_all();
 
 	for (unsigned int i = 0; i < m_threadCount; ++i)
 		m_threads[i].join();
-
-	delete[m_threadCount] m_threadSets;
 }
 
 void FileHasher::addNode(std::shared_ptr<MD5>& hash, std::shared_ptr<FilePointers>& filePointers)
 {
 	m_queue.push({ hash, filePointers });
-	m_runningCondition.notify_one();
 }
 
-void FileHasher::hashThread(ThreadSet& set)
+void FileHasher::hashThread(std::atomic<ThreadStatus>& status)
 {
 	std::mutex mutex;
 	std::unique_lock lk(mutex);
-	while (set.status != ThreadSet::QUIT)
+	do 
 	{
-		while (set.status == ThreadSet::ACTIVE)
+		status = ACTIVE;
+		while (auto opt = m_queue.pop_front())
 		{
-			if (m_queue.empty())
-				m_runningCondition.wait(lk);
-
-			while (auto opt = m_queue.pop_front())
-			{
-				HashNode& node = opt.value();
-				node.hash->generate(node.file->m_file, node.file->m_end);
-			}
+			HashNode& node = opt.value();
+			node.hash->generate(node.file->m_file, node.file->m_end);
 		}
 
-		if (set.status == ThreadSet::STOP)
-		{
-			set.status = ThreadSet::IDLE;
-			set.status.notify_one();
-		}
+		status = IDLE;
+		status.notify_one();
 
-		set.idleCondition.wait(lk);
-	}
+		m_condition.wait(lk);
+	} while (status != QUIT);
 }
 
 void FileHasher::startThreads()
 {
-	for (unsigned int i = 0; i < m_threadCount; ++i)
-	{
-		m_threadSets[i].status = ThreadSet::ACTIVE;
-		m_threadSets[i].idleCondition.notify_one();
-	}
+	m_queue.start();
+	m_condition.notify_all();
 }
 
 void FileHasher::stopThreads()
 {
+	m_queue.stop();
 	for (unsigned int i = 0; i < m_threadCount; ++i)
-		m_threadSets[i].status = ThreadSet::STOP;
-	m_runningCondition.notify_all();
-
-	for (unsigned int i = 0; i < m_threadCount; ++i)
-		m_threadSets[i].status.wait(ThreadSet::STOP);
+		m_statuses[i].wait(ACTIVE);
 }
