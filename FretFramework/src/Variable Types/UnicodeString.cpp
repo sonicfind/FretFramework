c654d1d5e22e5a2bd44ccfdf20f69ac99add31e2
sonicfind <sonicfind2@gmail.com>
2022-09-02 11:35:45 -0500

    UnicodeString: quicker casing
    Obvious issue: still only applies to basic ascii

--- FretFramework/src/Variable Types/UnicodeString.cpp
+++ FretFramework/src/Variable Types/UnicodeString.cpp
@@ -21,16 +21,17 @@ UnicodeString& UnicodeString::operator=(const std::u32string& str)
 	return *this;
 }
 
-constexpr void UnicodeString::setCasedStrings()
+constexpr void UnicodeString::setCasedStrings() noexcept
 {
-	m_string_lowercase = m_string;
-	m_string_uppercase = m_string;
-	for (size_t i = 0; i < m_string.size(); ++i)
+	m_string_lowercase = m_string_uppercase = m_string;
+	for (size_t i = 0; i < m_string_uppercase.size(); ++i)
 	{
-		if (65 <= m_string[i] && m_string[i] <= 90)
-			m_string_lowercase[i] += 32;
-		else if (97 <= m_string[i] && m_string[i] <= 122)
-			m_string_uppercase[i] -= 32;
+		const char32_t character = m_string_uppercase[i];
+		if (character < 128)
+		{
+			m_string_lowercase[i] = s_LOWER[character];
+			m_string_uppercase[i] = s_UPPER[character];
+		}
 	}
 }
 
