13044a3c71c6389932d23d513004ce21107c23eb
sonicfind <sonicfind2@gmail.com>
2022-09-11 21:10:39 -0500

    WebType: introduce WebType_t typedef

--- FretFramework/src/Variable Types/WebType.h
+++ FretFramework/src/Variable Types/WebType.h
@@ -7,4 +7,6 @@ namespace WebType
 	uint32_t read(const unsigned char*& dataPtr);
 	void copyToBuffer(const uint32_t value, char*& buffer);
 	void writeToFile(const uint32_t value, std::fstream& outFile);
-};
+
+	typedef uint32_t WebType_t;
+}
