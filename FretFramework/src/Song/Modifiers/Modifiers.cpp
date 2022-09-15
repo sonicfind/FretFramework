b968d2fa65ad5df620321abb766b2219f02fc831
sonicfind <sonicfind2@gmail.com>
2022-09-15 18:55:20 -0500

    Modifier: fix switch cases for writing

--- FretFramework/src/Song/Modifiers/Modifiers.cpp
+++ FretFramework/src/Song/Modifiers/Modifiers.cpp
@@ -15,14 +15,10 @@ void TxtFileModifier::write(std::fstream& outFile) const
 	outFile << '\t' << m_name << " = ";
 	switch (m_type)
 	{
-	case Type::STRING:        writeValue(*cast<UnicodeString>()); break;
 	case Type::STRING_NOCASE: writeValue(*cast<std::u32string>()); break;
-	case Type::UINT32:        writeValue(*cast<uint32_t     >()); break;
 	case Type::INT32:         writeValue(*cast<int32_t      >()); break;
 	case Type::UINT16:        writeValue(*cast<uint16_t     >()); break;
-	case Type::BOOL:          writeValue(*cast<bool         >()); break;
 	case Type::FLOAT:         writeValue(*cast<float        >()); break;
-	case Type::FLOATARRAY:    writeValue(*cast<FloatArray   >()); break;
 	}
 }
 
@@ -40,12 +36,13 @@ void TxtFileModifier::write_ini(std::fstream& outFile) const
 	outFile << m_name << " = ";
 	switch (m_type)
 	{
-	case Type::STRING:     writeValue(*cast<UnicodeString>()); break;
-	case Type::UINT32:     writeValue(*cast<uint32_t     >()); break;
-	case Type::INT32:      writeValue(*cast<int32_t      >()); break;
-	case Type::UINT16:     writeValue(*cast<uint16_t     >()); break;
-	case Type::BOOL:       writeValue(*cast<bool         >()); break;
-	case Type::FLOAT:      writeValue(*cast<float        >()); break;
-	case Type::FLOATARRAY: writeValue(*cast<FloatArray   >()); break;
+	case Type::STRING:        writeValue(*cast<UnicodeString>()); break;
+	case Type::STRING_NOCASE: writeValue(*cast<std::u32string>()); break;
+	case Type::UINT32:        writeValue(*cast<uint32_t     >()); break;
+	case Type::INT32:         writeValue(*cast<int32_t      >()); break;
+	case Type::UINT16:        writeValue(*cast<uint16_t     >()); break;
+	case Type::BOOL:          writeValue(*cast<bool         >()); break;
+	case Type::FLOAT:         writeValue(*cast<float        >()); break;
+	case Type::FLOATARRAY:    writeValue(*cast<FloatArray   >()); break;
 	}
 }
