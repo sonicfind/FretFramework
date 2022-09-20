de8fffb959690d317c1f180d27cc89a470d43b2a
sonicfind <sonicfind2@gmail.com>
2022-09-20 16:16:56 -0500

    Modifiers update
    + Explicitly define all typed constructors.
    + Cast() now returns a reference instead of a pointer
    + Made write_ini's lambda a private template function

--- FretFramework/src/Song/Modifiers/Modifiers.cpp
+++ FretFramework/src/Song/Modifiers/Modifiers.cpp
@@ -3,30 +3,20 @@
 void TxtFileModifier::write_cht(std::fstream& outFile) const
 {
 	if (m_type == Type::STRING_NOCASE)
-		outFile << '\t' << m_name << " = \"" << UnicodeString::U32ToStr(*cast<std::u32string>()) << "\"\n";
+		outFile << '\t' << m_name << " = \"" << UnicodeString::U32ToStr(cast<std::u32string>()) << "\"\n";
 }
 
 void TxtFileModifier::write_ini(std::fstream& outFile) const
 {
-	const auto writeValue = [&outFile](const auto& arg)
-	{
-		using T = std::decay_t<decltype(arg)>;
-		if      constexpr (std::is_same_v<T, bool>)           outFile << std::boolalpha << arg << '\n';
-		else if constexpr (std::is_same_v<T, FloatArray>)     outFile << arg[0] << ' ' << arg[1] << '\n';
-		else if constexpr (std::is_same_v<T, std::u32string>) outFile << UnicodeString::U32ToStr(arg) << '\n';
-		else                                                  outFile << arg << '\n';
-	};
-
-	outFile << m_name << " = ";
 	switch (m_type)
 	{
-	case Type::STRING:        writeValue(*cast<UnicodeString>()); break;
-	case Type::STRING_NOCASE: writeValue(*cast<std::u32string>()); break;
-	case Type::UINT32:        writeValue(*cast<uint32_t     >()); break;
-	case Type::INT32:         writeValue(*cast<int32_t      >()); break;
-	case Type::UINT16:        writeValue(*cast<uint16_t     >()); break;
-	case Type::BOOL:          writeValue(*cast<bool         >()); break;
-	case Type::FLOAT:         writeValue(*cast<float        >()); break;
-	case Type::FLOATARRAY:    writeValue(*cast<FloatArray   >()); break;
+	case Type::STRING:        writeVal<UnicodeString >(outFile); break;
+	case Type::STRING_NOCASE: writeVal<std::u32string>(outFile); break;
+	case Type::UINT32:        writeVal<uint32_t      >(outFile); break;
+	case Type::INT32:         writeVal<int32_t       >(outFile); break;
+	case Type::UINT16:        writeVal<uint16_t      >(outFile); break;
+	case Type::BOOL:          writeVal<bool          >(outFile); break;
+	case Type::FLOAT:         writeVal<float         >(outFile); break;
+	case Type::FLOATARRAY:    writeVal<FloatArray    >(outFile); break;
 	}
 }
--- FretFramework/src/Song/Modifiers/Modifiers.h
+++ FretFramework/src/Song/Modifiers/Modifiers.h
@@ -34,25 +34,34 @@ public:
 		new(c_BUFFER) std::u32string(std::move(string));
 	}
 
-	template <class T>
-	constexpr TxtFileModifier(const std::string_view name, T value) : m_name(name)
+	TxtFileModifier(const std::string_view name, uint32_t u32) : m_name(name), m_type(Type::UINT32)
 	{
-		if constexpr (std::is_same_v<T, uint32_t>)
-			m_type = Type::UINT32;
-		else if constexpr (std::is_same_v<T, int32_t>)
-			m_type = Type::INT32;
-		else if constexpr (std::is_same_v<T, uint16_t>)
-			m_type = Type::UINT16;
-		else if constexpr (std::is_same_v<T, bool>)
-			m_type = Type::BOOL;
-		else if constexpr (std::is_same_v<T, float>)
-			m_type = Type::FLOAT;
-		else if constexpr (std::is_same_v<T, FloatArray>)
-			m_type = Type::FLOATARRAY;
-		else
-			throw std::runtime_error("Invalid constructor type");
+		memcpy(c_BUFFER, &u32, sizeof(uint32_t));
+	}
 
-		memcpy(c_BUFFER, &value, sizeof(T));
+	TxtFileModifier(const std::string_view name, int32_t i32) : m_name(name), m_type(Type::INT32)
+	{
+		memcpy(c_BUFFER, &i32, sizeof(int32_t));
+	}
+
+	TxtFileModifier(const std::string_view name, uint16_t u16) : m_name(name), m_type(Type::UINT16)
+	{
+		memcpy(c_BUFFER, &u16, sizeof(uint16_t));
+	}
+
+	TxtFileModifier(const std::string_view name, bool bl) : m_name(name), m_type(Type::BOOL)
+	{
+		memcpy(c_BUFFER, &bl, sizeof(bool));
+	}
+
+	TxtFileModifier(const std::string_view name, float flt) : m_name(name), m_type(Type::FLOAT)
+	{
+		memcpy(c_BUFFER, &flt, sizeof(float));
+	}
+
+	TxtFileModifier(const std::string_view name, FloatArray fltArr) : m_name(name), m_type(Type::FLOATARRAY)
+	{
+		memcpy(c_BUFFER, &fltArr, sizeof(FloatArray));
 	}
 
 	TxtFileModifier(TxtFileModifier&& other) noexcept : m_name(other.m_name), m_type(other.m_type)
@@ -63,15 +72,15 @@ public:
 
 private:
 	template <class T>
-	T* cast()
+	T& cast()
 	{
-		return reinterpret_cast<T*>(c_BUFFER);
+		return *reinterpret_cast<T*>(c_BUFFER);
 	}
 
 	template <class T>
-	const T* cast() const
+	const T& cast() const
 	{
-		return reinterpret_cast<const T*>(c_BUFFER);
+		return *reinterpret_cast<const T*>(c_BUFFER);
 	}
 
 public:
@@ -82,8 +91,8 @@ public:
 		m_type = other.m_type;
 		switch (m_type)
 		{
-		case Type::STRING:        new(c_BUFFER) UnicodeString(*other.cast<UnicodeString>()); break;
-		case Type::STRING_NOCASE: new(c_BUFFER) std::u32string(*other.cast<std::u32string>()); break;
+		case Type::STRING:        new(c_BUFFER) UnicodeString(other.cast<UnicodeString>()); break;
+		case Type::STRING_NOCASE: new(c_BUFFER) std::u32string(other.cast<std::u32string>()); break;
 		default:
 			memcpy(c_BUFFER, other.c_BUFFER, sizeof(c_BUFFER));
 			break;
@@ -93,16 +102,16 @@ public:
 	
 	~TxtFileModifier() noexcept
 	{
-		static constexpr auto destruct = [](auto* ptr)
+		static constexpr auto destruct = [](auto& obj)
 		{
-			using T = std::decay_t<decltype(*ptr)>;
-			ptr->~T();
+			using T = std::decay_t<decltype(obj)>;
+			obj.~T();
 		};
 
 		if (m_type == Type::STRING)
-			destruct((UnicodeString*)c_BUFFER);
+			destruct(cast<UnicodeString>());
 		else if (m_type == Type::STRING_NOCASE)
-			destruct((std::u32string*)c_BUFFER);
+			destruct(cast<std::u32string>());
 	}
 
 	constexpr std::string_view getName() const noexcept { return m_name; }
@@ -130,7 +139,7 @@ public:
 		if (!validateType<T>())
 			throw std::runtime_error("Template type does match internal type");
 
-		return *reinterpret_cast<T*>(c_BUFFER);
+		return cast<T>();
 	}
 
 	template <class T>
@@ -139,7 +148,7 @@ public:
 		if (!validateType<T>())
 			throw std::runtime_error("Template type does match internal type");
 
-		return *reinterpret_cast<const T*>(c_BUFFER);
+		return cast<T>();
 	}
 
 	template <class T>
@@ -148,10 +157,20 @@ public:
 		if (!validateType<T>())
 			throw std::runtime_error("Template type does match internal type");
 
-		*reinterpret_cast<T*>(c_BUFFER) = value;
+		cast<T>() = value;
 		return *this;
 	}
 
 	void write_cht(std::fstream& outFile) const;
+
+	template <class T>
+	void writeVal(std::fstream& outFile) const
+	{
+		if      constexpr (std::is_same_v<T, bool>)           outFile << m_name << " = " << std::boolalpha << cast<T>() << '\n';
+		else if constexpr (std::is_same_v<T, FloatArray>)     outFile << m_name << " = " << cast<T>()[0] << ' ' << cast<T>()[1] << '\n';
+		else if constexpr (std::is_same_v<T, std::u32string>) outFile << m_name << " = " << UnicodeString::U32ToStr(cast<T>()) << '\n';
+		else                                                  outFile << m_name << " = " << cast<T>() << '\n';
+	}
+
 	void write_ini(std::fstream& outFile) const;
 };
